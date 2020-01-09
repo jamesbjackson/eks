f753d7a1cb331d93cda4c711395106af69c6b63c
James Jackson <>
2020-01-09 11:36:33 +0000

    Adding a Rancher Server instance

--- terraform/rancher/rancher_server.tf
+++ terraform/rancher/rancher_server.tf
@@ -8,26 +8,26 @@
 
 
 resource "kubernetes_namespace" "cattle_system" {
-    metadata {
-        name = "cattle-system"
-        labels = {
-            "app.kubernetes.io/managed-by" = "terraform",
-            "field.cattle.io/projectId" = "placeholder"
-        }
-        annotations = {
-            "cattle.io/status" = "placeholder"
-            "field.cattle.io/projectId" = "placeholder"
-            "lifecycle.cattle.io/create.namespace-auth" = "placeholder"
-        }
+  metadata {
+    name = "cattle-system"
+    labels = {
+        "app.kubernetes.io/managed-by" = "terraform",
+        "field.cattle.io/projectId" = "placeholder"
     }
-    lifecycle {
-        ignore_changes = [
-            metadata[0].annotations["cattle.io/status"],
-            metadata[0].labels["field.cattle.io/projectId"],
-            metadata[0].annotations["field.cattle.io/projectId"],
-            metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"]
-        ]
-    }    
+    annotations = {
+        "cattle.io/status" = "placeholder"
+        "field.cattle.io/projectId" = "placeholder"
+        "lifecycle.cattle.io/create.namespace-auth" = "placeholder"
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+        metadata[0].annotations["cattle.io/status"],
+        metadata[0].labels["field.cattle.io/projectId"],
+        metadata[0].annotations["field.cattle.io/projectId"],
+        metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"]
+    ]
+  }    
 }
 
 resource "helm_release" "rancher" {  
@@ -37,40 +37,19 @@ resource "helm_release" "rancher" {
   chart      = "rancher"
   version    = "2.2.6"
 
-  set {
-    name  = "hostname"
-    value = "rancher.connectedproducts.io"
-  }
-
-  set {
-    name  = "addLocal"
-    value = "auto"
-  }
-
-  set {
-    name  = "tls"
-    value = "external"
-  } 
-
-  set {
-    name  = "scope.ingressClass"
-    value = "alb"
-  }
-
-  set {
-    name  = "scope.singleNamespace"
-    value = "false"
-  }
-
-  set_string {
-    name  = "ingress.extraAnnotations.kubernetes.io/ingress.class"
-    value = ": alb"
-  }
-
-  set_string {
-    name  = "ingress.extraAnnotations.alb.ingress.kubernetes.io/scheme"
-    value = ": internet-facing"
-  }
+  values = [
+    <<-EOF
+      addLocal: auto
+      hostname: rancher.connectedproducts.io
+      ingress:
+        extraAnnotations:
+          alb.ingress.kubernetes.io/ssl-policy: ELBSecurityPolicy-TLS-1-2-Ext-2018-06
+          alb.ingress.kubernetes.io/certificate-arn: arn:aws:acm:eu-west-2:376405082911:certificate/62b0389b-1253-4ba4-9f1f-efd722beaeb2
+          alb.ingress.kubernetes.io/scheme: internet-facing
+          kubernetes.io/ingress.class: alb
+      tls: external
+    EOF
+  ]
 
   depends_on = [ 
     helm_release.aws_alb_ingress_controller,
